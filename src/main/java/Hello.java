b8b2cffee34b934ed1edef72e11f479962b4a53d
徐文川 <xuwenchuan@up366.com>
2020-11-03 18:44:09 +0800

    second commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ *@Description: 
+ *@throws
+ *@author 徐文川
+ *@date 2020/11/3
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+}
